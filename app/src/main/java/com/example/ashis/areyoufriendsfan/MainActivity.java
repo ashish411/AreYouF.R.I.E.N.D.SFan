c18a04e7df0998f95d4b007cc6f624b3c1f4eaa8
ashish411 <ashish.choudhary2702@gmail.com>
2016-09-25 13:17:21 +0530

    added only homepage now
    an app which contains various quesstions as well as music,pitcures

---
+++ app/src/main/java/com/example/ashis/areyoufriendsfan/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ashis.areyoufriendsfan;
+
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final MediaPlayer mp = MediaPlayer.create(this,R.raw.theme_song);
+        mp.start();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=(TextView) findViewById(R.id.textView_rplay);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/ufonts.com_gabriel-weiss-friends-font.ttf");
+        textView.setTypeface(typeface);
+        button_start=(Button) findViewById(R.id.button_start);
+        button_start.setTypeface(typeface);
+       button_start.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               mp.stop();
+           }
+       });
+    }
+
+
+}
+
